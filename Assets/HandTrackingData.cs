1d3e26da6a4e5ba1e5c4e1c4c901690b7c4f6ba0
Diogo Rosário <diogogrosario1008@gmail.com>
2023-03-14 11:32:36 +0000

    LEap motion device data

---
+++ Assets/HandTrackingData.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+public class HandTrackingData : MonoBehaviour
+{
+    public LeapProvider leapProvider;
+    
+    private void Update()
+    {
+        Hand _leftHand = Hands.Left;
+        Hand _rightHand = Hands.Right;
+        Hand _prioritisedHand = Hands.Left ?? Hands.Right;
+        Hand _specificHand = Hands.Get(Chirality.Left);
+        List<Hand> _allHands = Hands.Provider.CurrentFrame.Hands;
+
+        foreach (Hand _hand in _allHands)
+        {
+            //Use _hand to Explicitly get the specified fingers from it
+            Finger _thumb = _hand.GetThumb();
+            Finger _index = _hand.GetIndex();
+            Finger _middle = _hand.GetMiddle();
+            Finger _ring = _hand.GetRing();
+            Finger _pinky = _hand.GetPinky();
+
+            Debug.Log("Start logging, isLeft = " + _hand.IsLeft);
+            Debug.Log(_thumb.TipPosition);
+            Debug.Log(_index.TipPosition);
+            Debug.Log(_middle.TipPosition);
+            Debug.Log(_ring.TipPosition);
+            Debug.Log(_pinky.TipPosition);
+
+        }
+        
+    }
+
+}
