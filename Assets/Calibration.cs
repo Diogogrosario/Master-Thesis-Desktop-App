ffb9b6867ec981ce1173d60b31cbf30014efc0ec
Diogo Rosário <diogogrosario1008@gmail.com>
2023-04-17 15:53:08 +0100

    calibration, still malfunctioning

--- Assets/Calibration.cs
+++ Assets/Calibration.cs
@@ -6,16 +6,6 @@ using UnityEngine;
 
 public class Calibration : MonoBehaviour
 {
-    
-    public LeapProvider leapProvider;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    
     //Assume tracker is placed on a flat horizontal surface
     //We will assume tracker is in this position   -
     //                                           -   -
@@ -23,19 +13,28 @@ public class Calibration : MonoBehaviour
     //Coordinates from top to bottom -> z axis
     //Coordinates from left to right -> x axis
 
-    
-    //calculated from htc vive tracker specification sheet
-    private Vector3 peak1Offset = new Vector3(0,0.04f,0.035f);
-    private Vector3 peak2Offset = new Vector3(0.04f,0.04f,-0.035f);
-    private Vector3 peak3Offset = new Vector3(-0.04f,0.4f,-0.035f);
-
     //Offsets between hand and tracker position to calculate the offset after
     private Vector3 Offset1 = new Vector3(0, 0, 0);
     private Vector3 Offset2 = new Vector3(0, 0, 0);
     private Vector3 Offset3 = new Vector3(0, 0, 0);
-
-    private bool calibrated = false;
     
+    private bool calibrated = false;
+
+    private GameObject peak1;
+    private GameObject peak2;
+    private GameObject peak3;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        peak1 = GameObject.Find("Peak1");
+        peak2 = GameObject.Find("Peak2");
+        peak3 = GameObject.Find("Peak3");
+    }
+
+    // Update is called once per frame
+
     void Update()
     {
         //Calibrated dont need to do anything
@@ -51,37 +50,33 @@ public class Calibration : MonoBehaviour
             Debug.Log(Offset1);
             Debug.Log(Offset2);
             Debug.Log(Offset3);
-            GameObject.Find("Service Provider (XR)").GetComponent<LeapXRServiceProvider>().deviceOffsetYAxis = 0.5f;
-            GameObject.Find("Service Provider (XR)").GetComponent<LeapXRServiceProvider>().deviceOffsetZAxis = 0.5f;
+            Vector3 calibrationOffset = (Offset1 + Offset2 + Offset3) / 3;
+            Debug.Log((Offset1+Offset2+Offset3)/3);
+            GameObject.Find("Service Provider (XR)").GetComponent<LeapXRServiceProvider>().deviceOffsetYAxis = calibrationOffset.y;
+            GameObject.Find("Service Provider (XR)").GetComponent<LeapXRServiceProvider>().deviceOffsetZAxis = calibrationOffset.z;
             calibrated = true;
         }
         
-        List<Hand> _allHands = Hands.Provider.CurrentFrame.Hands;
         Vector3 handPosition = new Vector3(0,0,0);
-        foreach (Hand _hand in _allHands)
-        {
-            if (_hand.IsRight)
-            {
-                Finger _thumb = _hand.GetThumb();
-                handPosition = _thumb.TipPosition;
-            }
-
-        }
+        GameObject thumb;
+        thumb = GameObject.FindWithTag("Thumb");
+        if(thumb != null)
+            handPosition = thumb.transform.position;
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            Vector3 peak1Position = transform.position + peak1Offset;
+            Vector3 peak1Position = peak1.transform.position;
             Offset1 = handPosition - peak1Position;
             Debug.Log("1 was pressed");
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            Vector3 peak2Position = transform.position + peak2Offset;
+            Vector3 peak2Position = peak2.transform.position;
             Offset2 = handPosition - peak2Position;
             Debug.Log("2 was pressed");
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            Vector3 peak3Position = transform.position + peak3Offset;
+            Vector3 peak3Position = peak3.transform.position;
             Offset3 = handPosition - peak3Position;
             Debug.Log("3 was pressed");
         }
